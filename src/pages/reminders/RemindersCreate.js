1a1b21b06cb8a630859456f156fdd71c6047cf82
ivangochanin <ivangochanin@gmail.com>
2023-01-26 01:32:51 +0100

    add ckeditor

--- src/pages/reminders/RemindersCreate.js
+++ src/pages/reminders/RemindersCreate.js
@@ -11,6 +11,8 @@ import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 const RemindersCreate = () => {
 	const url = process.env.REACT_APP_API_URL;
@@ -178,19 +180,28 @@ const RemindersCreate = () => {
 						value={formData.order || ''}
 						onChange={(e) => handleData(e)}
 					/>
-					<TextField
-						name="content"
-						size="medium"
-						color="info"
-						disabled={loading}
-						label="CkEditor 5"
-						variant="standard"
-						fullWidth
-						value={formData.content || ''}
-						onChange={(e) => handleData(e)}
-					/>
+					
 				</InputWrapper>
-
+				<div >
+				<CKEditor
+                    editor={ ClassicEditor }
+                    data="<p>Hello from CKEditor 5!</p>"
+                    onReady={ editor => {
+                        // You can store the "editor" and use when it is needed.
+                        console.log( 'Editor is ready to use!', editor );
+                    } }
+                    onChange={ ( event, editor ) => {
+                        const data = editor.getData();
+                        console.log( { event, editor, data } );
+                    } }
+                    onBlur={ ( event, editor ) => {
+                        console.log( 'Blur.', editor );
+                    } }
+                    onFocus={ ( event, editor ) => {
+                        console.log( 'Focus.', editor );
+                    } }
+                />
+				</div>
 				<ButtonWrapper>
 					<Button
 						variant="contained"
