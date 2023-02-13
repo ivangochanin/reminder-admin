c31a439df8de0909d8e781ca4c9b061e133055ef
Dragan <dragansavic993@gmail.com>
2023-02-14 00:58:54 +0100

    Quill uploader prepared for implementation

--- src/configs/quill.js
+++ src/configs/quill.js
@@ -1,3 +1,7 @@
+import { Quill } from "react-quill";
+import ImageUploader from "quill-image-uploader";
+Quill.register("modules/imageUploader", ImageUploader);
+
 const quillModules = {
   toolbar: [
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -5,7 +9,19 @@ const quillModules = {
     [{ list: "ordered" }, { list: "bullet" }],
     [{ indent: "-1" }, { indent: "+1" }],
     ["clean", "code-block", "image"]
-  ]
+  ],
+  // toDo - upload file & return image url
+  imageUploader: {
+    upload: file => {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve(
+              "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/480px-JavaScript-logo.png"
+            );
+          }, 3500);
+        });
+      }
+    }
 }
 
 export {
